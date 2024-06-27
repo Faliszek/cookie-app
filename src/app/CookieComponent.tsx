720537f821106618130ec466f6067a3d6b6361f7
Paweł Falisz <pawlic7@gmail.com>
2024-06-27 23:03:03 +0200

    Expire cookie

--- src/app/CookieComponent.tsx
+++ src/app/CookieComponent.tsx
@@ -2,49 +2,59 @@
 
 import { useState, useEffect } from "react";
 import Cookies from "js-cookie";
-import dynamic from "next/dynamic";
 
 const COOKIE_KEY = "myCookie";
 
-type CookiePersistance = "persisted" | "not-persisted";
-export const NoSsr = () => {
-  const [cookieValue, setCookieValue] =
-    useState<CookiePersistance>("not-persisted");
+enum CookiePersistance {
+  Persisted = "Persisted",
+  NotPersisted = "NotPersisted",
+}
+const CookieComponent = () => {
+  const [cookieValue, setCookieValue] = useState<CookiePersistance | null>(
+    null
+  );
 
   const handleButtonClick = () => {
     const newValue =
-      cookieValue === "persisted" ? "not-persisted" : "persisted";
-    Cookies.set(COOKIE_KEY, newValue, { expires: 7 });
+      cookieValue === CookiePersistance.Persisted
+        ? CookiePersistance.NotPersisted
+        : CookiePersistance.Persisted;
+
+    Cookies.set(COOKIE_KEY, newValue, { expires: 7, sameSite: "Lax" });
     setCookieValue(newValue);
   };
 
   useEffect(() => {
-    setCookieValue(
-      (Cookies.get(COOKIE_KEY) as CookiePersistance) ?? "not-persisted"
-    );
+    const currentCookie = Cookies.get(COOKIE_KEY) as CookiePersistance;
+    window.postMessage({ currentCookie });
+    setCookieValue(currentCookie ?? null);
   }, []);
 
   return (
     <div className="flex flex-col items-center justify-center p-4 min-h-screen gap-8">
       <h1 className="text-center text-xl font-medium">
-        This app persist cookie once you click a button (1.0.1)
+        This app persist cookie once you click a button (1.0.2)
       </h1>
       <div className="font-light">
         <strong>Cookie: </strong> {cookieValue}
       </div>
 
-      <button
-        onClick={handleButtonClick}
-        className={`rounded-lg min-w-32 ${
-          cookieValue === "persisted" ? "bg-red-500" : "bg-green-500"
-        } px-4 py-2 transition-all duration-200 ease-linear text-white`}
-      >
-        {cookieValue === "persisted" ? "Remove" : "Save"}
-      </button>
+      <div className="min-h-12">
+        {cookieValue ? (
+          <button
+            onClick={handleButtonClick}
+            className={`rounded-lg min-w-32 ${
+              cookieValue === CookiePersistance.Persisted
+                ? "bg-red-500"
+                : "bg-green-500"
+            } px-4 py-2 transition-all duration-200 ease-linear text-white`}
+          >
+            {cookieValue === CookiePersistance.Persisted ? "Remove" : "Save"}
+          </button>
+        ) : null}
+      </div>
     </div>
   );
 };
 
-export default dynamic(() => Promise.resolve(NoSsr), {
-  ssr: false,
-});
+export default CookieComponent;
